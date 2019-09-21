c8996bce0143a0ec8be5f25767913ae3021f164d
diaoqingping <694874926@qq.com>
2019-09-21 21:50:58 +0800

    实时更新用户信息完成

--- back/utils/config.js
+++ back/utils/config.js
@@ -1,5 +1,5 @@
 var puburl = "http://localhost:8000/";
 var url = {
     login: puburl + 'admin/login',
-
+    userinfo:puburl+'admin/userinfo_get'
 }
--- back/utils/user.js
+++ back/utils/user.js
@@ -1,8 +1,18 @@
+/**
+ * 
+ */
+
 var user = {
     // 登录
     login: function (username, password, callback) {
         $.post(url.login, {user_name: username,password: password}, function (res) {
             callback(res);
-        })
+        });
+    },
+    //获取用户信息
+    getInfo:function(callback){
+        $.get(url.userinfo,function(res){
+            callback(res);
+        });
     }
 }
